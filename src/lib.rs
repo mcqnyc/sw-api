d2ad8bbb74106ffcaa77e55901a71121386c80a9
Michael Quinlan <mcqnyc@users.noreply.github.com>
2023-01-23 20:44:53 -0500

    Refactor command line input (#2)
    * improve error handling when too few arguments
    
    * refactor: add CLI error handling; split code into lib crate

---
+++ src/lib.rs
@@ -0,0 +1,76 @@
+use std::error::Error;
+use reqwest;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct ApiResponse {
+    visits: Vec<Visits>,
+}
+
+pub struct ApiParams {
+    domain: String,
+    start_date: String,
+    end_date: String,
+    country: String,
+}
+
+impl ApiParams {
+    pub fn build(args: &[String]) -> Result<ApiParams, &'static str> {
+        if args.len() <  4 {
+            return Err("not enough arguments")
+        }
+
+        let domain = args[1].clone();
+        let start_date = args[2].clone();
+        let end_date = args[3].clone();
+        let country = args[4].clone();
+
+        Ok(ApiParams { domain, start_date, end_date, country })
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Visits {
+    date: String,
+    visits: f32,
+}
+
+pub async fn run(api_params: ApiParams, api_key: &str) -> Result<(), Box<dyn Error>>{
+    let request_url = format!("http://api.similarweb.com/v1/website/{}/total-traffic-and-engagement/visits?api_key={}&start_date={}&end_date={}&country={}&granularity=monthly&main_domain_only=false&format=json", api_params.domain, api_key, api_params.start_date, api_params.end_date, api_params.country);
+    let response = reqwest::get(&request_url).await.unwrap();
+
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            match response.json::<ApiResponse>().await {
+                Ok(parsed) => write_data_to_csv(parsed, api_params),
+                Err(e) => println!("{}", e),
+            };
+        }
+        reqwest::StatusCode::UNAUTHORIZED => {
+            println!("Invalid API key, most likely");
+        }
+        other => {
+            panic!("Uh oh! Something unexpected happened: {:?}", other);
+        }
+    };
+
+    Ok(())
+}
+
+fn write_data_to_csv(json_data: ApiResponse, api_params: ApiParams) {
+    let mut wtr = csv::Writer::from_path("output.csv").unwrap();
+    wtr.write_record(&["Domain", "Country", "Date", "Visits"]).unwrap();
+
+    let domain = &api_params.domain;
+    let country = &api_params.country;
+
+    for visits in json_data.visits {
+        let date = &visits.date;
+        let visits = &visits.visits.to_string();
+
+        wtr.serialize(&[domain, country, date, visits]).unwrap();
+    }
+
+    wtr.flush().unwrap();
+    println!("Job done!")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
 use dotenv::dotenv;
-use std::env;
-use reqwest;
-use serde::{Deserialize, Serialize};
+use std::{env, process};
 
-#[derive(Debug, Deserialize, Serialize)]
-struct ApiResponse {
-    visits: Vec<Visits>,
-}
+use sw_api::{ApiParams, run};
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Visits {
-    date: String,
-    visits: f32,
-}
-
-// tokio let's us use "async" on our main function
+// tokio lets us use "async" on our main function
 #[tokio::main]
 async fn main() {
     dotenv().ok();
@@ -22,43 +11,15 @@ async fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let domain = &args[1];
-    let start_date = &args[2];
-    let end_date = &args[3];
-    let country = &args[4];
+    // let api_params = ApiParams::build(&args).unwrap_or_else(|err| {
+    let api_params = ApiParams::build(&args).unwrap_or_else(|err| {
+        println!("Problem passing arguments: {err}");
+        process::exit(1);
+    });
 
-    let request_url = format!("http://api.similarweb.com/v1/website/{}/total-traffic-and-engagement/visits?api_key={}&start_date={}&end_date={}&country={}&granularity=monthly&main_domain_only=false&format=json", domain, api_key, start_date, end_date, country);
-    let response = reqwest::get(&request_url).await.unwrap();
-
-    match response.status() {
-        reqwest::StatusCode::OK => {
-            match response.json::<ApiResponse>().await {
-                Ok(parsed) => write_data_to_csv(parsed, domain, country),
-                Err(e) => println!("{}", e),
-            };
-        }
-        reqwest::StatusCode::UNAUTHORIZED => {
-            println!("Invalid API key, most likely");
-        }
-        other => {
-            panic!("Uh oh! Something unexpected happened: {:?}", other);
-        }
-    };
-}
-
-fn write_data_to_csv(json_data: ApiResponse, domain: &str, country: &str) {
-    // Open a file for writing the CSV data
-    let mut wtr = csv::Writer::from_path("output.csv").unwrap();
-    wtr.write_record(&["Domain", "Country", "Date", "Visits"]).unwrap();
-
-    // Iterate over the JSON data and write it to the CSV file
-    for visits in json_data.visits {
-        let date = &visits.date;
-        let visits = &visits.visits.to_string();
-
-        wtr.serialize(&[domain, country, date, visits]).unwrap();
+    // run(api_params, &api_key).await.expect("TODO: panic message");
+    if let Err(e) = run(api_params, &api_key).await {
+        println!("Application error: {e}");
+        process::exit(1);
     }
-
-    wtr.flush().unwrap();
-    println!("Job done!")
 }
